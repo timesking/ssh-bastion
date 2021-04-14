bcda30b4022399127e28e5611791476d579aea7d
timesking <timesking@gmail.com>
2021-04-13 23:14:58 -0700

    replace ldap lib and add go mod

--- auth.go
+++ auth.go
@@ -1,43 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "golang.org/x/crypto/ssh"
-    ldap "github.com/tonnerre/go-ldap"
+	"fmt"
+	"log"
+
+	ldap "github.com/go-ldap/ldap/v3"
+	"golang.org/x/crypto/ssh"
 )
 
 func AuthUserPass(conn ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
-    perm := &ssh.Permissions{
-        Extensions: map[string]string{
-            "authType":     "password",
-            "password":     string(password),
-        },
-    }
+	perm := &ssh.Permissions{
+		Extensions: map[string]string{
+			"authType": "password",
+			"password": string(password),
+		},
+	}
 
-    if _, ok := config.Users[conn.User()]; ! ok {
-        return nil, fmt.Errorf("User Doesn't Exist in Config")
-    }
-    
-    if string(password) == "" {
-        // Blank password isn't handled properly by LDAP library, fail here.
-        return nil, fmt.Errorf("Blank Password Not Allowed")
-    }
+	if _, ok := config.Users[conn.User()]; !ok {
+		return nil, fmt.Errorf("User Doesn't Exist in Config")
+	}
 
-    if config.Global.AuthType == "ad" {
-        l, err := ldap.Dial("tcp", config.Global.LDAP_Server)
-        if err != nil {
-            log.Printf("LDAP Connect Failed: %s", err)
-            return nil, fmt.Errorf("LDAP Connect Failed: %s", err)
-        }
+	if string(password) == "" {
+		// Blank password isn't handled properly by LDAP library, fail here.
+		return nil, fmt.Errorf("Blank Password Not Allowed")
+	}
 
-        if err := l.Bind(fmt.Sprintf("%s@%s", conn.User(), config.Global.LDAP_Domain), string(password)); err != nil {
-            log.Printf("LDAP Bind Failed: %s", err)
-            return nil, fmt.Errorf("LDAP Bind Failed: %s", err)
-        }
+	if config.Global.AuthType == "ad" {
+		l, err := ldap.Dial("tcp", config.Global.LDAP_Server)
+		if err != nil {
+			log.Printf("LDAP Connect Failed: %s", err)
+			return nil, fmt.Errorf("LDAP Connect Failed: %s", err)
+		}
 
-        return perm, nil
-    } else {
-        return nil, fmt.Errorf("No Valid Auth Types")
-    }
+		if err := l.Bind(fmt.Sprintf("%s@%s", conn.User(), config.Global.LDAP_Domain), string(password)); err != nil {
+			log.Printf("LDAP Bind Failed: %s", err)
+			return nil, fmt.Errorf("LDAP Bind Failed: %s", err)
+		}
+
+		return perm, nil
+	} else {
+		return nil, fmt.Errorf("No Valid Auth Types")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/timesking/ssh-bastion
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.38.19
+	github.com/go-ldap/ldap/v3 v3.3.0
+	github.com/jessevdk/go-flags v1.5.0
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	gopkg.in/yaml.v2 v2.4.0
+)
